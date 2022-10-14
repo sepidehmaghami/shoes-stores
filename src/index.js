e82c9326437c5eb707bab5f783edf6a5505d716c
sepideh maghami <sepidehmaghamiit@gmail.com>
2022-10-14 21:15:40 +0330

    fixed bug

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import App from './App';
 import 'antd/dist/antd.css';
 // import 'antd/dist/antd.less'
 import AuthContextProvider from './context/auth-context';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-    <BrowserRouter >
+    <HashRouter >
       <App />
-      </BrowserRouter>
+      </HashRouter>
     </AuthContextProvider>
   </React.StrictMode>
 );
