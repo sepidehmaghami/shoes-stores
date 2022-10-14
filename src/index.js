0bc0f1a96917040caf45c09029de2a0624d466eb
sepideh maghami <sepidehmaghamiit@gmail.com>
2022-10-14 21:03:55 +0330

    fixed bug

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-    <HashRouter >
-      <App />
+      <HashRouter >
+        <App />
       </HashRouter>
     </AuthContextProvider>
   </React.StrictMode>
